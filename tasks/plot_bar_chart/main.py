c1b97930d2a4c3e4ecc1d9a40c0f0c05ae64f9dd
shaun <beingswu@gmail.com>
2024-08-29 16:00:14 +0800

    feat(flow): update flow

--- tasks/plot_bar_chart/main.py
+++ tasks/plot_bar_chart/main.py
@@ -18,7 +18,6 @@ def main(inputs: dict, context: Context):
   df = inputs.get("df")
   color = inputs.get("color")
 
-  hex_color = rgb_to_hex(color)
 
   title = inputs.get("title")
   x_column = inputs.get("x_column")
@@ -26,7 +25,7 @@ def main(inputs: dict, context: Context):
 
 
   plt.figure(figsize=(10, 6))
-  plt.bar(df[x_column], df[y_column], color=hex_color)
+  plt.bar(df[x_column], df[y_column], color=color)
   plt.xlabel(x_column)
   plt.ylabel(y_column)
   plt.title(title)
@@ -34,33 +33,5 @@ def main(inputs: dict, context: Context):
   plt.tight_layout()
 
   # plt.clf()
-
-  context.preview({
-    # type can be "image", "video", "audio", "markdown"
-    "type": "image",
-    # data can be file path, base64
-    "data": draw_to_base64(plt),
-  })
+  plt.show()
   return { "df": df }
-
-def rgb_to_hex(rgb):
-  rgb = rgb.replace("rgb(", "").replace(")", "").split(", ")
-  hex_color = "#{:02x}{:02x}{:02x}".format(int(rgb[0]), int(rgb[1]), int(rgb[2]))
-  
-  return hex_color
-
-def draw_to_base64(plt):
-  buffer = io.BytesIO()
-  plt.savefig(buffer, format='png')
-  buffer.seek(0)
-
-  # 从缓冲区创建一个Image对象
-  image = Image.open(buffer)
-
-  # 将Image对象转换为Base64编码的字符串
-  image_base64 = base64.b64encode(buffer.getvalue()).decode()
-
-  # 清除缓冲区和关闭缓冲区
-  buffer.close()
-
-  return image_base64
